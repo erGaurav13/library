2d50431cb3409042f79f6382646dfa49db7089ac
Gaurav kumar <ergauravkumar02071998@gmail.com>
2025-06-02 15:14:54 +0530

    GG

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(compression()); // To compress the response body
 // ✅ Allow all origins (temporary for development)
 app.use(
   CORS({
-    origin: ['http://localhost:5173', 'https://dashing-snickerdoodle-1fa68e.netlify.app'],
+    origin: ['http://localhost:5173', 'https://dashing-snickerdoodle-1fa68e.netlify.app','https://dulcet-monstera-b72319.netlify.app','https://stirring-gaufre-8667db.netlify.app/login'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
